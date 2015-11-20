915efd21f0794018a94221180bf3633c00d3bf91
Tim Repke <timmothey@gmx.de>
2015-11-20 20:16:45 +0100

    added jade for frontend error dumps

--- qbee/app.js
+++ qbee/app.js
@@ -10,6 +10,9 @@ var client = require('./routes/client');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
